5c1288541664376239b7eee28fc36986d5d71fda
Arif <arif.sor@gmail.com>
2020-08-15 10:49:51 -0400

    Generics and Event Handling

---
+++ Events/Event Handling/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Event_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataPushOperation<string> Data = new DataPushOperation<string>();
+            var Push = new PushOperation();
+
+            Data.datapush += Push.onPushData;
+
+            Data.pushdata("mehwish");
+            Data.pushdata("Arif");
+            Data.pushdata("Eshaal");
+            Data.pushdata("Rayan");
+
+            Console.WriteLine();
+        }
+    }
+
+    public class PushOperation
+    {
+        public void onPushData(object source, EventArgs e)
+        {
+            Console.WriteLine("Item is added to the Queue");
+        }
+
+    }
+
+    public class DataPushOperation<T> : EventArgs
+    {
+        public delegate void PushEventHandler(object source, EventArgs e);
+
+        public event PushEventHandler datapush;
+
+        private Queue<T> q = new Queue<T>();
+
+        public void pushdata(T item)
+        {
+            q.Enqueue(item);
+            onPushData();
+        }
+
+        protected virtual void onPushData()
+        {
+            if (datapush != null)
+                datapush(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> g = new Generic<int>();
+            g.Add(10);
+            g.Add(20);
+            g.Add(30);
+            g.Add(40);
+            Console.WriteLine(g.get());
+            Console.WriteLine(g.get());
+            Console.WriteLine(g.get());
+            Console.WriteLine(g.get());
+            Console.ReadLine();
+        }
+    }
+
+    public class Generic<T> where T : struct
+
+    {
+        private T[] myArr ;
+        private int position =0;
+        private int getposition=0;
+
+        public Generic()
+        {
+            myArr = new T[50];
+        }
+
+        public void Add(T item)
+        {
+            myArr[position] = item;
+            position++;
+        }
+
+        public T get ()
+        {
+          
+                T item = myArr[getposition];
+                getposition++;
+           
+                return item;
+        }
+      
+        }
+
+    }
+
